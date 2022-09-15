0e7a0f066f08d125a3acabec5eb93f2136e054ed
Estela Cordeiro Mateus <101813326+stelamateus@users.noreply.github.com>
2022-09-14 22:17:26 -0300

    variáveis para as funcionalidades js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const carrinho = document.querySelector('#addcar');
+const conteudoCarrinho = document.querySelector('#lista-carrinho tbody');
+const esvaziarrCarrinho = document.querySelector('#esvaziar-carrinho');
+const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrinho = [];
