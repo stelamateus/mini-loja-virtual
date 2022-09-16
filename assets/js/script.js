84e9ead23ccf920a4d6a75392362e82d74144cf5
Estela Cordeiro Mateus <101813326+stelamateus@users.noreply.github.com>
2022-09-15 22:07:00 -0300

    adicionando pasta js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,125 @@
-const carrinho = document.querySelector('#addcar');
+const carrinho = document.querySelector('#carrinho');
 const conteudoCarrinho = document.querySelector('#lista-carrinho tbody');
-const esvaziarrCarrinho = document.querySelector('#esvaziar-carrinho');
+const esvaziarCarrinho = document.querySelector('#esvaziar-carrinho');
 const listaCursos = document.querySelector('#lista-cursos');
 let articulosCarrinho = [];
+
+cargarEventListeners();
+    
+    function cargarEventListeners(){
+        
+         listaCursos.addEventListener('click', agregarCurso);
+        
+         carrinho.addEventListener('click',eliminarCurso);
+
+         esvaziarCarrinho.addEventListener('click', () => {
+             articulosCarrinho = [];
+
+             limparHTML(); 
+         })
+    }
+        
+
+        function agregarCurso(e){
+            e.preventDefault();
+            if(e.target.classList.contains('agregar-carrinho')){
+                const cursoSeleccionado = e.target.parentElement.parentElement;
+                leerDatosCurso(cursoSeleccionado);
+    
+            }
+        }
+    
+        
+    
+        function eliminarCurso(e) {
+    
+            if(e.target.classList.contains('borrar-curso')){
+                const cursoId = e.target.getAttribute('data-id')
+    
+                
+                articulosCarrinho = articulosCarrinho.filter(curso => curso.id !== cursoId);
+    
+        
+                carrinhoHTML();
+    
+            }  
+        }
+    
+        
+    
+        function leerDatosCurso(curso){
+    
+            
+    
+            const infoCurso = {
+                imagen: curso.querySelector('img').src,
+                titulo: curso.querySelector('h4').textContent,
+                preço: curso.querySelector('.preço span').textContent,
+                id:curso.querySelector('a').getAttribute('data-id'),
+                quantidade: 1
+            }
+    
+          
+            const existe = articulosCarrinho.some( curso => curso.id === infoCurso.id);
+    
+            if(existe){
+            
+               const cursos= articulosCarrinho.map(curso => 
+                {
+    
+                    if(curso.id === infoCurso.id){
+                        curso.quantidade++;
+                        return curso;
+                    }else{
+                        return curso; 
+                    }
+                });
+    
+               articulosCarrinho = [...cursos];
+    
+            }else{
+                
+                articulosCarrinho = [...articulosCarrinho,infoCurso];
+                  
+            }
+    
+            carrinhoHTML();
+    
+        }
+    
+    
+    
+        function carrinhoHTML(){
+    
+            //limpar html
+            limpaarHTML();
+    
+    
+            
+            articulosCarrinho.forEach( curso =>{
+                const {imagen,titulo,preço,quantidade,id} = curso
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                <td><img src="${imagen}" width="100"></td> 
+                <td>${titulo}</td> 
+                <td> ${preço}</td>
+                <td> ${quantidade}</td>
+                <td><a href="#" class="borrar-curso" data-id="${id}"> x </td>
+                `;
+                
+
+    
+                conteudoCarrinho.appendChild(row);
+    
+            })
+    
+        }
+    
+
+    
+        function limparHTML(){
+    
+            while(conteudoCarrinho.firstChild){
+                conteudoCarrinho.removeChild(conteudoCarrinho.firstChild)
+            }
+        }
